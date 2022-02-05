efbf9063c5ff38eb6b894544afb9c596c01f87fe
saintly2k <ninefreaks@yandex.com>
2022-02-05 18:21:10 +0100

    fix perms on schedule page

--- core/arrays.req.php
+++ core/arrays.req.php
@@ -4,11 +4,11 @@
 
 $noNav = array("signin","signup","confirm","forgot","changelog","news","forum","settings","add_anime","edit_anime","add_episode","edit_episode");
 $controlNav = array("watchlist","follows");
-$scheduleNav = array("schedule","mod");
+$scheduleNav = array("schedule","newest","mod");
 $watchNav = array("watch");
 $followNav = array("anime");
 $requireLogin = array("settings","follows","forum");
-$requireMod = array("new_forum","edit_forum","add_anime","edit_anime","add_episode","edit_episode");
+$requireMod = array("new_forum","edit_forum","add_anime","edit_anime","add_episode","edit_episode","mod");
 $requireAdmin = array("system");
 
 ?>
